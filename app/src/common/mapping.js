39ac8713320c862bc142bd74162f4c609f43fd80
Nikita <VTB4110972@region.vtb.ru>
2024-12-11 18:57:42 +0300

    feat: mapping departments

--- app/src/common/mapping.js
+++ app/src/common/mapping.js
@@ -1,6 +1,6 @@
 const DEPARTMENTS = {
   KIB_SMB: "Управление моделирования КИБ и СМБ",
-  PARTNERSHIPS_IT: "Управление моделирования партнерств и ИТ процессов",
+  PARTNERSHIPS_IT: "Управление моделирования партнерств и ИТ-процессов",
   RB: "Управление моделирования РБ",
   ML_ALGORITHMS: "Управление перспективных алгоритмов машинного обучения",
   PROCESS_FINANCIAL: "Управление процессных и финансовых моделей",
@@ -18,7 +18,7 @@ const DEPARTMENT_TO_STREAM_MAPPING = {
   [DEPARTMENTS.KIB_SMB]: STREAMS.KIB_SMB,
   [DEPARTMENTS.PARTNERSHIPS_IT]: STREAMS.PARTNERSHIPS_IT,
   [DEPARTMENTS.RB]: STREAMS.RB,
-  [DEPARTMENTS.ML_ALGORITHMS]: STREAMS.ML_ALGORITHМС,
+  [DEPARTMENTS.ML_ALGORITHMS]: STREAMS.ML_ALGORITHMS,
   [DEPARTMENTS.PROCESS_FINANCIAL]: STREAMS.PROCESS_FINANCIAL,
 };
 
