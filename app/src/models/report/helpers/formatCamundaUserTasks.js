695a450e9b119953e692f77abae8d5bd6d2af221
Nik Elin <67843454+nik1999777@users.noreply.github.com>
2025-05-12 12:45:40 +0300

    feat: получение активных задач пользователей

--- app/src/models/report/helpers/formatCamundaUserTasks.js
+++ app/src/models/report/helpers/formatCamundaUserTasks.js
@@ -3,9 +3,10 @@ const { user_roles } = require("../constants");
 const formatUserTasks = (userTasks) =>
   userTasks.reduce((allTasks, groupTask, index) => {
     const formattedGroupTasks = groupTask.map(
-      ({ name, processInstanceId }) => ({
+      ({ name, processInstanceId, assignee }) => ({
         name,
         processInstanceId,
+        assignee,
         role: user_roles[index],
       })
     );
--- app/src/models/report/sql/assigneeHistByModelIds.js
+++ app/src/models/report/sql/assigneeHistByModelIds.js
@@ -1,47 +1,45 @@
 const sql = `
-SELECT M.MODEL_ID,
-       M.MODEL_NAME,
-       M.ROOT_MODEL_ID,
-       M.MODEL_VERSION,
-       TO_CHAR(M_UPD_DATE.UPDATE_DATE, 'DD.MM.YYYY HH24:MI:SS') AS UPDATE_DATE,
-       coalesce(M.MODEL_STAGE, STATUS.BPMN_KEY_DESC)            AS STATUS,
-       AH.FUNCTIONAL_ROLE,
-       STRING_AGG(AH.ASSIGNEE_NAME, ', ')                          AS ASSIGNEE_NAME
+SELECT 
+  M.MODEL_ID,
+  M.MODEL_NAME,
+  M.ROOT_MODEL_ID,
+  M.MODEL_VERSION,
+  TO_CHAR(M_UPD_DATE.UPDATE_DATE, 'DD.MM.YYYY HH24:MI:SS') AS UPDATE_DATE,
+  COALESCE(M.MODEL_STAGE, STATUS.BPMN_KEY_DESC) AS STATUS,
+  AH.FUNCTIONAL_ROLE,
+  AH.ASSIGNEE_NAME
 FROM ASSIGNEE_HIST AH
-         LEFT JOIN MODELS M
-                   ON AH.MODEL_ID = M.MODEL_ID
-         LEFT JOIN (
-    SELECT U_M.MODEL_ID, MAX(U_M.EFFECTIVE_FROM) AS UPDATE_DATE
-    FROM (
-             SELECT MODEL_ID, EFFECTIVE_FROM
-             FROM ARTEFACT_REALIZATIONS
-         ) U_M
-    GROUP BY U_M.MODEL_ID
+LEFT JOIN MODELS M ON AH.MODEL_ID = M.MODEL_ID
+LEFT JOIN (
+  SELECT MODEL_ID, MAX(EFFECTIVE_FROM) AS UPDATE_DATE
+  FROM ARTEFACT_REALIZATIONS
+  GROUP BY MODEL_ID
 ) M_UPD_DATE ON M.MODEL_ID = M_UPD_DATE.MODEL_ID
-         LEFT JOIN (
-    SELECT *
-    FROM (
-             SELECT MODEL_ID,
-                    BPMN_KEY_DESC,
-                    ROW_NUMBER() OVER (
-                        PARTITION BY BI.model_id
-                        ORDER BY bi.effective_to DESC, bi.effective_from DESC, bi.bpmn_key_id DESC
-                        ) AS rn
-             FROM BPMN_INSTANCES BI
-                      LEFT JOIN BPMN_PROCESSES BP
-                                ON BI.BPMN_KEY_ID = BP.BPMN_KEY_ID
-                                    AND effective_to = TO_TIMESTAMP('9999-12-3123:59:59', 'YYYY-MM-DDHH24:MI:SS')
-         ) as dummy
-    WHERE rn = 1
+LEFT JOIN (
+  SELECT * FROM (
+    SELECT 
+      MODEL_ID,
+      BPMN_KEY_DESC,
+      ROW_NUMBER() OVER (
+        PARTITION BY BI.model_id 
+        ORDER BY bi.effective_to DESC, bi.effective_from DESC, bi.bpmn_key_id DESC
+      ) AS rn
+    FROM BPMN_INSTANCES BI
+    LEFT JOIN BPMN_PROCESSES BP 
+      ON BI.BPMN_KEY_ID = BP.BPMN_KEY_ID
+      AND effective_to = TO_TIMESTAMP('9999-12-31 23:59:59', 'YYYY-MM-DD HH24:MI:SS')
+  ) dummy
+  WHERE rn = 1
 ) STATUS ON STATUS.MODEL_ID = M.MODEL_ID
 WHERE AH.MODEL_ID = ANY(:modelIds::text[])
-  AND TO_TIMESTAMP(cast(M_UPD_DATE.UPDATE_DATE as TEXT), 'YYYY-MM-DD HH24:MI:SS') <= TO_DATE(:dateOfDelay, 'YYYY-MM-DD HH24:MI:SS')
-  AND BPMN_KEY_DESC IN ('main', 'initialization', 'data', 'data_search', 'data_pilot', 'data_build', 'model',
-                        'model_validation', 'integration', 'integration_datamart', 'integration_env_conf',
-                        'integration_test', 'integration_user', 'integration_prod', 'cancel')
-group by m.MODEL_ID, M.ROOT_MODEL_ID, M.MODEL_VERSION, AH.FUNCTIONAL_ROLE, M.MODEL_NAME, STATUS.BPMN_KEY_DESC,
-         M_UPD_DATE.UPDATE_DATE
-ORDER BY M_UPD_DATE.UPDATE_DATE desc
+  AND TO_TIMESTAMP(CAST(M_UPD_DATE.UPDATE_DATE AS TEXT), 'YYYY-MM-DD HH24:MI:SS') 
+      <= TO_DATE(:dateOfDelay, 'YYYY-MM-DD HH24:MI:SS')
+  AND BPMN_KEY_DESC IN (
+    'main', 'initialization', 'data', 'data_search', 'data_pilot', 'data_build', 'model',
+    'model_validation', 'integration', 'integration_datamart', 'integration_env_conf',
+    'integration_test', 'integration_user', 'integration_prod', 'cancel'
+  )
+ORDER BY M_UPD_DATE.UPDATE_DATE DESC
 `;
 
 module.exports = sql;
