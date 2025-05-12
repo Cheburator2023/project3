e6dd5da736531399f6cf744efaf03c184e1fd911
Pavel Smetanin <psmetanin@dev.vtb.ru>
2025-05-12 14:49:51 +0300

    feat: add required SSL connection to DB

--- app/src/connectors/database/postgres/config.js
+++ app/src/connectors/database/postgres/config.js
@@ -1,16 +1,19 @@
 module.exports = {
-    poolConfig: {
-        user: process.env.PGUSER,
-        password: process.env.PGPASSWORD,
-        host: process.env.PGHOST,
-        port: process.env.PGPORT,
-        database: process.env.PGSCHEMA,
-        idleTimeoutMillis: parseInt(process.env.POOL_QUEUE_TIMEOUT) || 10000,
-        max: parseInt(process.env.POOL_MAX) || 50,
-        allowExitOnIdle: false
+  poolConfig: {
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGSCHEMA,
+    idleTimeoutMillis: parseInt(process.env.POOL_QUEUE_TIMEOUT) || 10000,
+    max: parseInt(process.env.POOL_MAX) || 50,
+    allowExitOnIdle: false,
+    ssl: {
+      rejectUnauthorized: false,
     },
-    retryConfig: {
-        retries: parseInt(process.env.ATTEMPT) || 3,
-    },
-    callTimeout: parseInt(process.env.CALL_TIMEOUT) || 10 * 10000,
+  },
+  retryConfig: {
+    retries: parseInt(process.env.ATTEMPT) || 3,
+  },
+  callTimeout: parseInt(process.env.CALL_TIMEOUT) || 10 * 10000,
 };
